3f28e8e6f0fa795ce680ca0a37327a62d86e02d5
Kristian Weegink <k.weegink@gmail.com>
2024-09-26 00:18:09 +1000

    raylib build: TODO change linking

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -std=c11 -pedantic -o sms main.c
+gcc -std=c11 -pedantic -Iraylib-5.0_linux_amd64/include/ -lm -o sms main.c raylib-5.0_linux_amd64/lib/libraylib.a
 if [[ $? == 0 ]]; then
     ./sms
 fi
